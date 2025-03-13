98c485da966ea2310b6396a610cf109b1534d46e
TheDougMiester <dorsal_fin1@yahoo.com>
2025-03-13 16:07:56 -0600

    Update main.cpp
    Updates to demo code to align with changes to the decoder file

--- esp32-rmt-pwm-decoder/src/main.cpp
+++ esp32-rmt-pwm-decoder/src/main.cpp
@@ -1,122 +1,87 @@
 #include "esp32-rmt-pwm-decoder.h"
-#include "RidiculouslySmallDfPlayer.h"
-#include "EyeMovements.h"
-//#include "MSGEQ7.h"
 #include <Arduino.h>
+#include "esp_timer.h"
 
 RxDecoder rfReceiver;
-static const uint8_t RF_RECEIVER_PIN = GPIO_NUM_11;
+static const gpio_num_t RF_RECEIVER_PIN = GPIO_NUM_11;
 
-//DF Player
-static const uint8_t DFPLAYER_TX = GPIO_NUM_37;  
-static const uint8_t DFPLAYER_RX = GPIO_NUM_36; 
-static const uint8_t DFPLAYER_BUSY = GPIO_NUM_38;
-static const uint8_t DFPLAYER_DAC_L = GPIO_NUM_41; // for FFT processing
-static const uint32_t DFPLAYER_BAUD = 9600;
-RidiculouslySmallDfPlayer dfPlayer;
-HardwareSerial DfPlayerSerial(2);
-
-#if 0
-//MSGEQ7
-static const uint8_t  MSGEQ7_MULTIPLEX_OUT = GPIO_NUM_7; // msgeq7 pin 3
-static const uint8_t MSGEQ7_STROBE_PIN = GPIO_NUM_16 ; // msgeq7 pin 4
-//static const uint8_t  MSGEQ7_AUDIO_IN_FROM_DFPLAYER = 0; // not needed
-static const uint8_t MSGEQ7_RESET_PIN = GPIO_NUM_15;  // msgeq7 pin 7
-static const uint8_t MSGEQ7_OSC_CLK_PIN = GPIO_NUM_6; // msgeq7 pin 8
-static const uint32_t MSGEQ7_OSC_CLK_FREQ = 160000; //a number in range of optimal freq of msgeq7
-                                                    // divisible by 80MHz, thus avoiding jitter.
-MSGEQ7 msgeq7;
-#endif
-
-//eyes
-static const uint8_t LEFT_EYE = 21;
-static const uint8_t RIGHT_EYE = 47;
-EyeMovements ledEyes;
+volatile uint8_t fifty_hz_timer_flag = 0;
+#define FLAG_50HZ 0 
+static void fifty_hz_timer_callback(void *arg) {
+  /* This pushes handling into loop(). For this reason avoid using delay() */
+ /* Waits may be implemented with countdowns in loop() using the 50Hz tick */
+  fifty_hz_timer_flag |= (1 << FLAG_50HZ);
+}
+static esp_timer_handle_t fifty_hz_timer_handle = NULL;
+const esp_timer_create_args_t fifty_hz_timer_config = {
+  .callback = &fifty_hz_timer_callback, // The callback function to be executed
+  .arg = NULL, // Optional argument to pass to the callback
+  .name = "fifty_hz_timer" // Optional name for debugging
+};
 
 void setup() {
   Serial.begin(115200);
   //rf receiver
   rfReceiver.setRxPin(RF_RECEIVER_PIN);
 
-  //df player
-  DfPlayerSerial.begin(DFPLAYER_BAUD, SERIAL_8N1, DFPLAYER_RX, DFPLAYER_TX);
-  dfPlayer.begin(DfPlayerSerial, DFPLAYER_BUSY);
-  //I don't know if I need these delays...
-  delay(3000);
-  dfPlayer.resetPlayer();
-  delay(3000);
-  dfPlayer.setVolume(1);
-  
-  ledEyes.setEyePins(LEFT_EYE,RIGHT_EYE);
-  //let the user know the system is online.
-  ledEyes.bothEyesOn();
-  delay(2000);
-  ledEyes.eyesOff();
-
-
-#if 0
-  msgeq7.initialize(MSGEQ7_MULTIPLEX_OUT /*msgeq7 pin 3*/, MSGEQ7_STROBE_PIN /*msgeq7 pin 4*/,
-                    MSGEQ7_RESET_PIN /* msgeq7 pin 7*/ , MSGEQ7_OSC_CLK_PIN /*pin 8 */, 
-                    MSGEQ7_OSC_CLK_FREQ /*in hz*/);
-
-  msgeq7.beginOscClock();
-#endif
-  delay(5000);
+  delay(5000); // not really needed, but it leaves time to look at the serial port to watch everything get initialized
   xTaskCreatePinnedToCore(rfReceiver.rxSignalHandler, "rxSignalHandler", 4096, NULL, 10, NULL, 1);
+
+  esp_timer_create(&fifty_hz_timer_config, &fifty_hz_timer_handle);
+  esp_timer_start_periodic(fifty_hz_timer_handle, 20000); // 50hz - 5 times per second
   Serial.printf("setup complete\n"); 
 }
 
 uint32_t decoded;
 bool isBusy = false;
-uint8_t dude = 0;
-void loop() {
-  isBusy = dfPlayer.isBusy();
-  if(isBusy) {
-    ledEyes.bothEyesOn();    
-    dude = 1;
-  } else if (!(isBusy) && (dude == 1)) {
-    ledEyes.eyesOff();
-    Serial.printf("player is done\n");
-    dude = 0;
-  }
 
-  if (rfReceiver.available()) {
-    decoded = rfReceiver.getReceivedValue();
-    switch (decoded) {   //translate fob codes (first 4)
-      case 0x4C8568:
-        Serial.printf("button 1\n");
-        dfPlayer.playTrack(1);
-        break;
-      case 0x4C8564:
-        Serial.printf("button 2\n");   
-        dfPlayer.playTrack(2);       
-        break;
-      case 0x4C856C:
-        Serial.printf("button 3\n");
-        dfPlayer.playTrack(3);
-        break;
-      case 0x4C8562:
-        Serial.printf("button 4\n");
-        dfPlayer.playTrack(4);
-        break;
-      case 0x4C856A  :
-        Serial.printf("button 5\n");
-        break;
-      case 0x4C8566:
-        Serial.printf("button 6\n");          
-        break;
-      case 0x4C856E:
-        Serial.printf("button 7\n");
-        break;
-      case 0x4C8561:
-        Serial.printf("button 8\n");
-        dfPlayer.stopPlaying();
-        break;
-      default:
-        Serial.printf("Unknown signal: 0x%X bit length: %d\n", 
-          rfReceiver.getReceivedValue(), rfReceiver.getReceivedBitlength() );
+void loop() {
+
+  if (fifty_hz_timer_flag & (1 << FLAG_50HZ)){// runs 50 times per sec
+    fifty_hz_timer_flag &= ~(1 << FLAG_50HZ); //reset.
+
+    if (rfReceiver.available()) {
+      decoded = rfReceiver.getReceivedValue();
+      switch (decoded) {   //translate fob codes (first 4)
+        case 0x4C8568:
+          Serial.printf("button 1\n");    
           break;
-    } // end of switch
-    rfReceiver.resetAvailable();
-  }
-}
+        case 0x4C8564:
+          Serial.printf("button 2\n");      
+          break;
+        case 0x4C856C:
+          Serial.printf("button 3\n");      
+          break;
+        case 0x4C8562:
+          Serial.printf("button 4\n");
+          Serial.printf("## code: 0x%X bit length: %d\n", 
+            rfReceiver.getReceivedValue(), rfReceiver.getReceivedBitlength() );    
+          break;
+        case 0x4C856A  :
+          Serial.printf("button 5\n");
+          Serial.printf("## code: 0x%X bit length: %d\n", 
+            rfReceiver.getReceivedValue(), rfReceiver.getReceivedBitlength() );          
+          break;
+        case 0x4C8566:
+          Serial.printf("button 6\n");          
+          break;
+        case 0x4C856E:
+          Serial.printf("button 7\n");
+          break;
+        case 0x4C8561:
+          Serial.printf("button 8\n");
+          Serial.printf("## code: 0x%X bit length: %d\n", 
+            rfReceiver.getReceivedValue(), rfReceiver.getReceivedBitlength() );
+          delay(50); // give the player 50 ms to stop
+          break;
+        default:
+          Serial.printf("Unknown signal: 0x%X bit length: %d\n", 
+            rfReceiver.getReceivedValue(), rfReceiver.getReceivedBitlength() );
+            break;
+      } // end of switch
+      rfReceiver.resetAvailable();    
+    } //if receiver
+
+  } //if (fifty_hz_timer_flag
+  vTaskDelay(500); // non-blocking delay. Not needed if more stuff is happening in loop
+}  //loop
